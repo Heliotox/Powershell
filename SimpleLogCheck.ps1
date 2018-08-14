1d2e357a0b1b39cd5ab7dacf24d30a5bde2c19d6
Greg Lahti <git@greglahti.com>
2018-08-14 10:45:17 -0500

    Update to Simple Log Check

--- SimpleLogCheck.ps1
+++ SimpleLogCheck.ps1
@@ -10,14 +10,44 @@ if (!(Test-Path "C:\Temp\Incomplete.txt"))
    New-Item -path C:\Temp\ -name Incomplete.txt
 }
 
+if (!(Test-Path "C:\Temp\NoLog.txt"))
+{
+   New-Item -path C:\Temp\ -name NoLog.txt
+}
+
+if (!(Test-Path "C:\Temp\Offline.txt"))
+{
+   New-Item -path C:\Temp\ -name Offline.txt
+}
+
 
 
 
 ForEach ($Computer in $Computers)
     {
-        $temp = $Computer.Name
-        Write-host "Checking $temp"
-        $LogLine = gc \\$temp\C$\Temp\NGUpgrade.log -Tail 1
-        If ($LogLine -like '*Complete*') {Out-File -FilePath C:\Temp\Complete.txt -Append -InputObject $temp}
-        Else {Out-File -FilePath C:\Temp\Incomplete.txt -Append -InputObject $temp}
+    $temp = $Computer.Name
+    Write-host "Checking if $temp is Online"
+         If (Test-Connection -ComputerName $temp -Count 1 -quiet)
+            {
+            Write-host "$temp is online."
+                if(Test-Path \\$temp\C$\Temp\NGUpgrade.log)
+                    {
+                        Write-host "Checking $temp"
+                        $LogLine = gc \\$temp\C$\Temp\NGUpgrade.log -Tail 1
+                        If ($LogLine -like '*Complete*') 
+                            {
+                                Write-Host "$temp Complete."
+                                Out-File -FilePath C:\Temp\Complete.txt -Append -InputObject $temp}
+                        Else 
+                            {
+                                Write-Host "$temp Incomplete."
+                                Out-File -FilePath C:\Temp\Incomplete.txt -Append -InputObject $temp}
+                    }
+                Else {
+                Write-Host "Log File on $temp does not exist."
+                Out-File -FilePath C:\Temp\NoLog.txt -Append -InputObject $temp}
+            }
+         Else
+         {Write-Host "$temp is not online."
+         Out-File -FilePath C:\Temp\Offline.txt -Append -InputObject $temp}
     }
